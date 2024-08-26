66d0ec1957f9027e2ad1de430c7a4e2f84cfc2aa
Aleksi <liukkonenaleksi@gmail.com>
2024-08-26 12:57:51 +0300

    Add select styles

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -7,16 +7,17 @@ interface FilterProps {
 
 const Filter: React.FC<FilterProps> = ({ sortOrder, setSortOrder }) => {
   return (
-    <div>
+    <form>
       <select
         onChange={(e) => setSortOrder(e.target.value)}
         value={sortOrder}
-      >
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5">
+      
         <option value="default">Sort by</option>
         <option value="low-to-high">Lowest Price First</option>
         <option value="high-to-low">Highest Price First</option>
       </select>
-    </div>
+    </form>
   );
 };
 
