86fa4a7d6f1085afcba7612e49b19af4b4b7e7e4
Aleksi <liukkonenaleksi@gmail.com>
2024-08-26 10:41:49 +0300

    Handle loading and add loadingspinner

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-
+import LoadingSpinner from './LoadingSpinner';
 
 interface Product {
     id: number;
@@ -10,28 +10,44 @@ interface Product {
 }
 
 const ProductList = () => {
-    const [products, setProducts] = useState<Product[]>([]);
+    const [state, setState] = useState<{ products: Product[]; loading: boolean; error: string | null }>({
+        products: [],
+        loading: true,
+        error: null,
+    });
 
     useEffect(() => {
         const fetchProducts = async () => {
-            const response = await fetch('https://fakestoreapi.com/products');
-            const data = await response.json();
-            console.log(data)
-            setProducts(data);
+            try {
+                const response = await fetch('https://fakestoreapi.com/products');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setState({ products: data, loading: false, error: null });
+            } catch (error) {
+                setState({ products: [], loading: false, error: (error instanceof Error) ? error.message : 'An unknown error occurred' });
+            }
         };
         fetchProducts();
     }, []);
 
+    if (state.loading) {
+        return <LoadingSpinner />;
+    }
 
+    if (state.error) {
+        return <div>Error: {state.error}</div>;
+    }
 
     return (
         <div>
-            {products.map(product => (
+            {state.products.map(product => (
                 <div key={product.id}>
                     <img src={product.image} alt={product.title} />
                     <h2>{product.title}</h2>
                     <p>${product.price}</p>
-                    <button onClick={() => alert('Jeah')}>Add to Cart</button>
+                    <button onClick={() => alert('Added to Cart')}>Add to Cart</button>
                 </div>
             ))}
         </div>
