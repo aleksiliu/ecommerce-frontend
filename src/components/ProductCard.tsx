aff1eeb4fb35985713a40d849a763831eacb3f4c
Aleksi <liukkonenaleksi@gmail.com>
2024-08-27 09:48:58 +0300

    Add favorite icon

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import type { Product } from '../types';
 
+
 const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
   return (
-    <div className="overflow-hidden rounded-lg transform shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+    <div className="relative overflow-hidden rounded-lg transform shadow-md duration-300 hover:scale-105 hover:shadow-lg group">
       <div className="relative w-full h-48">
         <img
           className="absolute inset-0 w-full h-full object-contain object-center"
           src={product.image}
           alt={product.title}
         />
+        <div className="absolute top-2 right-2 p-2 rounded-full bg-white opacity-100 sm:opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          <button onClick={() => alert('Added to Favorites!')}className="p-2 bg-white rounded-full shadow hover:bg-gray-100">
+            <svg className="w-6 h-6 text-blue-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+            </svg>
+          </button>
+        </div>
       </div>
       <div className="p-4 flex flex-col justify-between h-48">
         <div>
