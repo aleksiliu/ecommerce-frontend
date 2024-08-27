6d11e069aac104f667f65be064de0fe390707e17
Aleksi <liukkonenaleksi@gmail.com>
2024-08-27 09:50:30 +0300

    Update heart color

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -13,7 +13,7 @@ const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
         />
         <div className="absolute top-2 right-2 p-2 rounded-full bg-white opacity-100 sm:opacity-0 group-hover:opacity-100 transition-opacity duration-300">
           <button onClick={() => alert('Added to Favorites!')}className="p-2 bg-white rounded-full shadow hover:bg-gray-100">
-            <svg className="w-6 h-6 text-blue-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <svg className="w-6 h-6 text-slate-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
             </svg>
           </button>
