a96ce595a27923b4522654c7b9c2c896ccf90d06
Aleksi <liukkonenaleksi@gmail.com>
2024-08-28 14:32:27 +0300

    Update skeleton

--- src/components/SkeletonLoader.jsx
+++ src/components/SkeletonLoader.jsx
@@ -1,11 +1,20 @@
 const SkeletonLoader = () => {
   return (
-    <div className="animate-pulse overflow-hidden rounded-lg transform shadow-md duration-300">
+    <div className="bg-white relative overflow-hidden rounded-lg transform shadow-md duration-300 hover:scale-105 hover:shadow-lg">
+      {/* Image placeholder */}
       <div className="relative w-full h-48 bg-gray-300 rounded-t-lg"></div>
-      <div className="p-4">
-        <div className="h-4 bg-gray-300 rounded w-3/4 mb-4"></div>
-        <div className="h-4 bg-gray-300 rounded w-1/2 mb-4"></div>
-        <div className="mt-auto w-full h-10 bg-gray-300 rounded"></div>
+      
+      {/* Content placeholder */}
+      <div className="p-4 flex flex-col justify-between h-48">
+        <div className="space-y-4">
+          <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
+          <div className="h-4 bg-gray-300 rounded w-1/2 mb-2"></div>
+        </div>
+        
+        <div className='flex justify-between gap-4'>
+          <div className="h-10 bg-gray-300 rounded w-1/3"></div>
+          <div className="h-10 bg-gray-300 rounded w-1/3"></div>
+        </div>
       </div>
     </div>
   );
