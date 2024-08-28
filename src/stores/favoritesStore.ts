f3eafa9934e79a9faccb1c8c95f923c9c23cbdcb
Aleksi <liukkonenaleksi@gmail.com>
2024-08-28 12:45:58 +0300

    Prevent existing favorite

--- src/stores/favoritesStore.ts
+++ src/stores/favoritesStore.ts
@@ -6,5 +6,15 @@ export const $favorites = atom<Product[]>([]);
 
 // Function to add a product to favorites
 export const addToFavorites = (product: Product) => {
-  $favorites.set([...$favorites.get(), product]);
+  const currentFavorites = $favorites.get();
+
+  // Check if the product is already in the favorites
+  const isAlreadyFavorite = currentFavorites.some(fav => fav.id === product.id);
+
+  if (!isAlreadyFavorite) {
+    // If not already in favorites, add it
+    $favorites.set([...currentFavorites, product]);
+  } else {
+    alert(`${product.title} is already in your favorites.`);
+  }
 };
