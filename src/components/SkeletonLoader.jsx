2bfe318661a1669cfa713d12558180fb2200d9f7
Aleksi <liukkonenaleksi@gmail.com>
2024-08-28 14:32:43 +0300

    Remove comments

--- src/components/SkeletonLoader.jsx
+++ src/components/SkeletonLoader.jsx
@@ -1,10 +1,9 @@
 const SkeletonLoader = () => {
   return (
     <div className="bg-white relative overflow-hidden rounded-lg transform shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-      {/* Image placeholder */}
+
       <div className="relative w-full h-48 bg-gray-300 rounded-t-lg"></div>
-      
-      {/* Content placeholder */}
+  
       <div className="p-4 flex flex-col justify-between h-48">
         <div className="space-y-4">
           <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
